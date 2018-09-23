2f38e3e8d23d40861886f0d3a3ff42ff7e440b10
Marcus Mathiassen <mathiassenmarcus@me.com>
2018-09-23 18:01:11 +0200

    Removed xcode project. Removed Build script. Added Cmake to generate both.

---
+++ build/CMakeFiles/Thi.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Thi.dir/src/ast.c.o"
+  "CMakeFiles/Thi.dir/src/codegen.c.o"
+  "CMakeFiles/Thi.dir/src/context.c.o"
+  "CMakeFiles/Thi.dir/src/globals.c.o"
+  "CMakeFiles/Thi.dir/src/lexer.c.o"
+  "CMakeFiles/Thi.dir/src/list.c.o"
+  "CMakeFiles/Thi.dir/src/main.c.o"
+  "CMakeFiles/Thi.dir/src/map.c.o"
+  "CMakeFiles/Thi.dir/src/parser.c.o"
+  "CMakeFiles/Thi.dir/src/stack.c.o"
+  "CMakeFiles/Thi.dir/src/string.c.o"
+  "CMakeFiles/Thi.dir/src/typespec.c.o"
+  "CMakeFiles/Thi.dir/src/utility.c.o"
+  "CMakeFiles/Thi.dir/src/value.c.o"
+  "../bin/Thi.pdb"
+  "../bin/Thi"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Thi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
