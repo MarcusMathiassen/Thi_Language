23f73a7539f50de7c7e3f89bdb385ed0fbc56ca5
Marcus Mathiassen <mathiassenmarcus@me.com>
2018-10-23 17:50:47 +0200

    fmt

--- src/utility.c
+++ src/utility.c
@@ -263,16 +263,15 @@ const char* get_unique_color(void) { return get_next_color(); }
 //                               Timing Utility Functions
 //------------------------------------------------------------------------------
 
-
-/* 
+/*
 author: jbenet
-os x, compile with: gcc -o testo test.c 
+os x, compile with: gcc -o testo test.c
 linux, compile with: gcc -o testo test.c -lrt
 */
 
-#include <time.h>
-#include <sys/time.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <time.h>
 
 #ifdef __MACH__
 #include <mach/clock.h>
@@ -286,15 +285,15 @@ f64 get_time(void)
     struct timespec ts;
 
 #ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
-  clock_serv_t cclock;
-  mach_timespec_t mts;
-  host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
-  clock_get_time(cclock, &mts);
-  mach_port_deallocate(mach_task_self(), cclock);
-  ts.tv_sec = mts.tv_sec;
-  ts.tv_nsec = mts.tv_nsec;
+    clock_serv_t cclock;
+    mach_timespec_t mts;
+    host_get_clock_service(mach_host_self(), CALENDAR_CLOCK, &cclock);
+    clock_get_time(cclock, &mts);
+    mach_port_deallocate(mach_task_self(), cclock);
+    ts.tv_sec = mts.tv_sec;
+    ts.tv_nsec = mts.tv_nsec;
 #else
-  clock_gettime(CLOCK_REALTIME, &ts);
+    clock_gettime(CLOCK_REALTIME, &ts);
 #endif
     s = ts.tv_sec;
     ms = ts.tv_nsec / 1.0e6;
