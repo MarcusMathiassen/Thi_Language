1c0d0cedc0f94727c9fc5f068c1469d7e2e3d358
Marcus Mathiassen <mathiassenmarcus@me.com>
2019-05-21 23:52:51 +0200

    ...

--- build_dev.bash
+++ build_dev.bash
@@ -21,11 +21,11 @@ if [ $? == 0 ]; then
     # ./test.bash ./tests/test_order_independance.thi
     # ./test.bash ./tests/test_pointer.thi
     # ./test.bash ./tests/test_var_args.thi
-    ./test.bash ./ tests/test_basic_setup.thi
+    # ./test.bash ./ tests/test_basic_setup.thi
     # ./test.bash ./ tests/test_typeof.thi
     # ./test.bash ./ tests/test_sizeof.thi
     # ./test.bash ./ tests/test_sizeof_2.thi
-    # ./test.bash ./tests/test_window.thi
+    ./test.bash ./tests/test_window.thi
     # ./test.bash ./tests/test_advanced_pointer.thi
     # ./test.bash ./tests/test_subscript_access.thi
     # ./test.bash ./tests/test_subscript_access_2.thi
