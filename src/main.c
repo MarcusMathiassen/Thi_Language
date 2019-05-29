3324b8c39467a52c2ea375f56a7054e0493ec373
Marcus Mathiassen <mathiassenmarcus@me.com>
2019-05-29 21:00:27 +0200

    Sema handles unordered decls correctly

--- src/main.c
+++ src/main.c
@@ -228,7 +228,7 @@ void constant_fold_unary(AST* node) {
         switch (op) {
         default: ERROR_UNHANDLED_KIND(token_kind_to_str(op));
         case TOKEN_BANG:    value = !oper_v; break;
-        case TOKEN_PLUS:    value = oper_v;  break;
+        case TOKEN_PLUS:    value =  oper_v;  break;
         case TOKEN_TILDE:   value = ~oper_v; break;
         case TOKEN_MINUS:   value = -oper_v; break;
         }
@@ -246,6 +246,27 @@ void constant_fold_binary(AST* node) {
     if (lhs->kind == AST_INT && rhs->kind == AST_INT) {
         s64 lhs_v = lhs->Int.val;
         s64 rhs_v = rhs->Int.val;
+
+        // Ignore 0
+        if (lhs_v == 0) {
+            switch(op) {
+            case TOKEN_MINUS: // fallthrough
+            case TOKEN_PLUS: 
+                ast_replace(node, rhs); 
+                break;
+            }
+        }
+        if (rhs_v == 0) {
+            switch(op) {
+            case TOKEN_MINUS: // fallthrough
+            case TOKEN_PLUS: 
+                ast_replace(node, lhs); 
+                break;
+            }
+        }
+
+
+
         s64 value = 0;
         // clang-format off
         switch (op) {
@@ -275,6 +296,28 @@ void constant_fold_binary(AST* node) {
     } else if (lhs->kind == AST_FLOAT && rhs->kind == AST_FLOAT) {
         f64 lhs_v = lhs->Float.val;
         f64 rhs_v = rhs->Float.val;
+
+
+        // Ignore 0.0
+        if (lhs_v == 0.0) {
+            switch(op) {
+            case TOKEN_MINUS: // fallthrough
+            case TOKEN_PLUS: 
+                ast_replace(node, rhs); 
+                break;
+            }
+        }
+        if (rhs_v == 0.0) {
+            switch(op) {
+            case TOKEN_MINUS: // fallthrough
+            case TOKEN_PLUS: 
+                ast_replace(node, lhs); 
+                break;
+            }
+        }
+
+
+
         f64 value = 0.0;
         // clang-format off
         switch (op) {
